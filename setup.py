04e5b9697561903c61d6751e6742b7ef190b8221
captaindro <pedroberbel@hotmail.com>
2025-07-04 22:16:24 +0200

    Make 'utils' installable and fix CI imports

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name="nono",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="."),  # cherche utils/ et tests/ comme packages
     include_package_data=True,
 )
