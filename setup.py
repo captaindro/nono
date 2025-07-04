ef63afcbbce90a58d9769f847cfa43be1a8f489f
captaindro <pedroberbel@hotmail.com>
2025-07-04 22:57:41 +0200

    Install nono as a package and fix CI imports

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="nono",
     version="0.1.0",
-    packages=find_packages(),      # détecte utils/ et tests/ comme packages
+    packages=find_packages(),      # détecte utils/ et tests/
     include_package_data=True,
 )
